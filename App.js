6c7673bf906e27b71fc1ec13708679a0eaad32b9
Revathy Nattamai <revathy.gopinath@wipro.com>
2019-03-31 21:52:04 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,35 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text, ScrollView, TouchableOpacity, View, StyleSheet } from 'react-native';
+import moment from 'moment';
+import faker from 'faker';
+import { transform } from './helpers';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, WebView } from 'react-native';
+export default class App extends Component {
+  state = {
+    transactions: []
+  };
+  componentDidMount() {
+    const fromDate = moment().subtract(1, 'months').startOf('month');
+    const toDate = moment().endOf('month');
+    let fakeData = { transactions: [] };
+    const randomTransactionTypes = ["DD", "SO", "BP", "DEB", "FPI", "FPO", "FEE", "TFR", "CPT", "MPO"];
+    const randomMerchants = [];
+    const randomAmounts = [];
+    for (let i = 0; i < 25; i++) {
+      randomMerchants[i] = faker.company.companyName();
+      randomAmounts[i] = faker.commerce.price();
+    }
+    const randomPaymentTypes = ['money_in', 'money_out'];
+    const randomize = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+    for (let i = 0; i < 2000; i++) {
+      fakeData.transactions.push({
+        "date": faker.date.between(moment(fromDate, 'DD-MM-YYYY'), moment(toDate, 'DD-MM-YYYY')),
+        "description": randomize(randomMerchants),
+        "payment_type": randomize(randomTransactionTypes),
+        [randomize(randomPaymentTypes)]: randomize(randomAmounts),
+        "balance": faker.commerce.price(),
+      });
+    }
+    this.setState({
+      transactions: fakeData.transactions,
+    });
+  }
+  buttonList(a){
+    return(<TouchableOpacity onPress={this.handlPress}>
+      <Text style={{ border: {a}, padding: 10, textAlign:'center', color: 'black', fontSize: 24 }}>Click Me!</Text>
+      </TouchableOpacity>);
+  }
   render() {
+    if (!this.state.transactions.length) return null;
+    const transformedData = transform(this.state.transactions);
     return (
-      <WebView
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        scrollEnabled={true}
-        bounce={false}
-        useWebKit={true}
-        source={{ uri: "https://www.lloydsbank.com" }}
-        originWhitelist={["*"]}
-        style={{ height: '100%', marginTop: 30, backgroundColor: '#ffffff' }} />
+      // <Button title="Learn More" color="black" />
+      // <ScrollView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      //   {transformedData.map(transaction => <Text>{transaction.keyword}</Text>)}
+      // </ScrollView>
+      <View>{this.buttonList('#000000')}</View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'blue',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 12,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    padding: 12,
+    textAlign:'center',
+  }
+})
